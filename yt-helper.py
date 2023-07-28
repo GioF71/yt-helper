4d73420bb03fc32cff65acf9a66ec162caf9a03c
GioF71 <giovanni.fulco@gmail.com>
2023-07-28 13:34:47 +0200

    handle upcoming

--- yt-helper.py
+++ yt-helper.py
@@ -3,12 +3,13 @@ import sys
 import string
 import time
 import copy
+import logging
 
 import persistence
 
-from resolution import Resolution
-
 import yt_dlp
+import exiftool
+import pytube
 from pytube import YouTube, Playlist, StreamQuery, Stream
 from pytube.exceptions import AgeRestrictedError
 from mutagen.mp4 import MP4, MP4MetadataError, MP4StreamInfoError
@@ -16,10 +17,10 @@ from mutagen.mp4 import MP4, MP4MetadataError, MP4StreamInfoError
 from functools import cmp_to_key
 
 def get_max_resolution() -> str: return os.getenv("MAX_RESOLUTION", "1080")
-def get_output_format() -> str: return os.getenv("OUTPUT_FORMAT", "mp4")
+def get_output_format() -> str: return os.getenv("OUTPUT_FORMAT", "mkv")
 
 def get_file_name_template() -> str:
-    return os.getenv("FILE_NAME_TEMPLATE", "$title.$subtype")
+    return os.getenv("FILE_NAME_TEMPLATE", "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s.%(ext)s")
 
 def get_subtype() -> str: return os.getenv("SUBTYPE", "mp4")
 
@@ -35,7 +36,7 @@ def compare_str(left : str, right : str) -> int:
     if left < right: return -1
     return 1
 
-def store_tags(ytdlp : yt_dlp.YoutubeDL, info_dict : dict[str, any]):
+def store_tags(yt : yt_dlp.YoutubeDL, info_dict : dict[str, any]):
     if get_output_format() == "mp4":
         file_name : str = info_dict["filename"]
         try: 
@@ -50,74 +51,71 @@ def store_tags(ytdlp : yt_dlp.YoutubeDL, info_dict : dict[str, any]):
         #tags["\\xa9day"] = str(yt.publish_date.year) #TODO use upload_date which is for example 20230510
         tags.save(file_name)
 
-filename_info_dict : dict[str, dict[str, any]] = {}
-
 def yt_dlp_monitor(d):
     status = d["status"]
     if status == "finished":
         file_name : str = d["filename"]
         print(f"Status [{status}] FileName [{file_name}]")
 
-class MyCustomPP(yt_dlp.postprocessor.PostProcessor):
+class PostProcessor(yt_dlp.postprocessor.PostProcessor):
     def run(self, info):
         self.to_screen(f"File name is [{info['filename']}]")
-        filename_info_dict[info["webpage_url"]] = copy.deepcopy(info)
+        #file_name : str = info["filename"]
+        #tool : exiftool.ExifToolHelper = exiftool.ExifToolHelper(file_name)
+        #metadata = tool.get_metadata(file_name)
         return [], info
 
 def process_url(url : str):
     if not persistence.has_been_downloaded(url):
-        #full_filename_path : str = os.path.join(get_output_path(), video_filename)
+        params : dict[str, any] = dict()
+        params["outtmpl"] = os.path.join(get_output_path(), get_file_name_template())
+        max_resolution : str = get_max_resolution()
+        params["format"] = f"bv*[height<={max_resolution}]+ba" if max_resolution else f"bv+ba"
+        if get_output_format(): params["merge_output_format"] = get_output_format()
+        params["writethumbnail"] = True
+        params["embedthumbnail"] = True
+        params["progress_hooks"] = [yt_dlp_monitor]
+        
         ydl_opts = {
-            "outtmpl": os.path.join(get_output_path(), "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s.%(ext)s"),
+            "outtmpl": os.path.join(get_output_path(), get_file_name_template()),
             "format": f"bv*[height<={get_max_resolution()}]+ba",
             "merge_output_format": get_output_format(),
             'writethumbnail': True,
             'embedthumbnail': True,
-            "progress_hooks": [yt_dlp_monitor]
-        }        
-        ytdlp : yt_dlp.YoutubeDL = yt_dlp.YoutubeDL(ydl_opts)
-        ytdlp.add_post_processor(MyCustomPP(), when='after_move')
-        ytdlp.download(url)
-        # TODO add tags
-        info_dict : dict[str, any] = filename_info_dict[url]
-        # TODO does not work ### store_tags(ytdlp, info_dict)
-        persistence.store_download_url(url)
-        del filename_info_dict[url]
-        
-        #yt : YouTube = YouTube(url)
-        #title = yt.title
-        #author = yt.author
-        #publish_date = yt.publish_date
-        #age_restricted : bool = yt.age_restricted
-        #if age_restricted:
-        #    print(f"Video is age restricted, skipping")
-        #    return
-        #print(f"Downloading [{title}] by [{author}] on [{publish_date}]...")
-        #stream : Stream = select_stream(yt)
-        #if stream:
-        #    print(f"Selected format: res:{stream.resolution} subtype:{stream.subtype} video_codec:{stream.video_codec} audio_codec:{stream.audio_codec}")
-        #    # Download if not exists
-        #    publish_date : str = f"{yt.publish_date.year:04d}-{yt.publish_date.month:02d}-{yt.publish_date.day:02d}"
-        #    video_filename : str = string.Template(get_file_name_template()).substitute(
-        #        title = yt.title,
-        #        author = yt.author,
-        #        publish_date = publish_date,
-        #        subtype = stream.subtype)
-        #    full_filename_path : str = os.path.join(get_output_path(), video_filename)
-        #    file_path : str = None
-        #    if os.path.exists(full_filename_path):
-        #        print(f"File already exists [{full_filename_path}] for url [{url}]")
-        #    else:
-        #        file_path : str = stream.download(
-        #            output_path = get_output_path(), 
-        #            filename = video_filename)
-        #        full_filename_path = file_path
-        #    # store tags
-        #    store_tags(yt, stream, full_filename_path)
-        #    # save url to db
-        #    persistence.store_download_url(url)
-        #else:
-        #    print(f"ERROR: Could not find a matching stream for [{url}]")
+            "progress_hooks": [yt_dlp_monitor],
+            #'postprocessors': [{
+            #    'key': 'MetadataParser',
+            #    'when': 'pre_process',
+            #    'actions': [
+            #        (yt_dlp.MetadataParserPP.Actions.INTERPRET, 'author', r'(?s)(?P<meta_comment>.+)'),
+            #        (yt_dlp.MetadataParserPP.Actions.INTERPRET, 'artist', r'(?s)(?P<meta_comment>.+)'),
+            #        (yt_dlp.MetadataParserPP.Actions.INTERPRET, 'aART', r'(?s)(?P<meta_comment>.+)')
+            #        
+            #    ]
+            #},
+            #]
+        }
+        pytube_yt : pytube.YouTube = pytube.YouTube(url)
+        skip_video : bool = False
+        skip_reason : str = None
+     
+        is_upcoming : bool = pytube_yt.vid_info["videoDetails"]["isUpcoming"] if "videoDetails" in pytube_yt.vid_info and "isUpcoming" in pytube_yt.vid_info["videoDetails"] else False
+        if is_upcoming:
+            skip_video = True
+            skip_reason = "Video is marked as \"upcoming\""
+        #playability : dict[str, any] = pytube_yt.vid_info["playabilityStatus"]
+        #print(f"Playability status for url:[{url}] Author:[{pytube_yt.author}] Title:[{pytube_yt.title}] is: [{playability['status']}]")
+        if not skip_video:
+            yt : yt_dlp.YoutubeDL = yt_dlp.YoutubeDL(params = params)
+            yt.add_post_processor(PostProcessor(), when='after_move')
+            try:
+                yt.download(url)
+                # TODO add tags
+                persistence.store_download_url(url)
+            except yt_dlp.utils.DownloadError as e:
+                print(f"Cannot download URL [{url}] due to [{type(e).__name__}] [{e.message if hasattr(e, 'message') else e}]")
+        else:
+            print(f"Skipping video at url [{url}], reason: [{skip_reason}]")
     else:
         print(f"Already downloaded [{url}]")
 
@@ -135,12 +133,13 @@ def main():
             playlist_url : str = build_playlist_url(current_playlist)
             print(f"Playlist id: {playlist_url}")
             playlist : Playlist = Playlist(playlist_url)
-            print(f"Playlist title: {playlist.title}")
-            url_list : list[str] = playlist.video_urls
-            url : str
-            for url in url_list:
-                print(f"Found url [{url}]")
-                process_url(url)
+            if playlist and len(playlist) > 0:
+                print(f"Playlist title: {playlist.title}")
+                url_list : list[str] = playlist.video_urls
+                url : str
+                for url in url_list if url_list else []:
+                    print(f"Found url [{url}]")
+                    process_url(url)
         if is_loop_enabled:
             print(f"Sleeping ...")     
             time.sleep(60)
