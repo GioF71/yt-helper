0c1bf9c56550bbd356f8c45de46166b4f6be2c68
GioF71 <giovanni.fulco@gmail.com>
2023-08-04 20:13:26 +0200

    add channels table

--- yt-helper/persistence.py
+++ yt-helper/persistence.py
@@ -36,20 +36,38 @@ def __prepare_table_playlists():
     cursor_obj.execute(create_table)
     cursor_obj.close()
 
+def __prepare_table_channels():
+    cursor_obj = __connection.cursor()
+    # Creating table
+    create_table : str = """
+        CREATE TABLE IF NOT EXISTS channels (
+        channel_id INTEGER PRIMARY KEY,
+        identifier_type VARCHAR(32) NOT NULL,
+        identifier_value VARCHAR(255) NOT NULL,
+        date_limit VARCHAR(32))
+    """
+    cursor_obj.execute(create_table)
+    create_index : str = """
+        CREATE UNIQUE INDEX IF NOT EXISTS idx_channel_identifier 
+        ON channels (identifier_type, identifier_value)"""
+    cursor_obj.execute(create_index)
+    cursor_obj.close()
+
 __connection : sqlite3.Connection = __get_connection()
 __prepare_table_downloads()
 __prepare_table_playlists()
+__prepare_table_channels()
     
 def __get_downloads_by_url(url : str):
     cursor = __connection.cursor()
-    cursor.execute("SELECT * FROM downloads WHERE download_url = ?", (url,))
+    cursor.execute("SELECT download_url, download_date FROM downloads WHERE download_url = ?", (url,))
     rows = cursor.fetchall()
     cursor.close()
     return rows
 
 def __get_playlist_by_id(id : str):
     cursor = __connection.cursor()
-    cursor.execute("SELECT * FROM playlists WHERE playlist_id = ?", (id,))
+    cursor.execute("SELECT playlist_id, date_limit FROM playlists WHERE playlist_id = ?", (id,))
     rows = cursor.fetchall()
     cursor.close()
     return rows
@@ -74,8 +92,11 @@ def store_playlist(playlist : Playlist):
         cursor.close()
         __connection.commit()
     else:
-        tuple = (start, playlist.playlist_id)
-        cursor = __connection.cursor()
-        cursor.execute("UPDATE playlists SET date_limit = ? WHERE playlist_id = ?", tuple)
-        cursor.close()
-        __connection.commit()
+        # anything different?
+        existing_start : str = existing[0][1]
+        if existing_start != start:
+            tuple = (start, playlist.playlist_id)
+            cursor = __connection.cursor()
+            cursor.execute("UPDATE playlists SET date_limit = ? WHERE playlist_id = ?", tuple)
+            cursor.close()
+            __connection.commit()
